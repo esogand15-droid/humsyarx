d670f0d844351f5b35399421f35393e774766eb7
Amir Hossein Bahmanzade <amirxboxlive84@gmail.com>
2026-09-12 02:46:43 +0330

    hellp

--- api/request_context.py
+++
@@ -1,4 +0,0 @@
-"""Cross-layer request correlation without coupling DB/domain code to FastAPI."""
-from contextvars import ContextVar
-
-current_request_id: ContextVar[str | None] = ContextVar("humsyar_request_id", default=None)
